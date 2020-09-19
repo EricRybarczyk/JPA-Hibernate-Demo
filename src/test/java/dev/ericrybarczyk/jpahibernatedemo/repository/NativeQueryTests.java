aa8ddf088481534567e249f2436b5b1288e4c716
EricRybarczyk <EricRybarczyk@gmail.com>
2020-09-19 18:42:58 -0400

    Added sample Native Queries demonstrated in Unit Tests

---
+++ src/test/java/dev/ericrybarczyk/jpahibernatedemo/repository/NativeQueryTests.java
@@ -0,0 +1,57 @@
+package dev.ericrybarczyk.jpahibernatedemo.repository;
+
+import dev.ericrybarczyk.jpahibernatedemo.JpaHibernateDemoApplication;
+import dev.ericrybarczyk.jpahibernatedemo.entity.Course;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.transaction.annotation.Transactional;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(classes = JpaHibernateDemoApplication.class)
+public class NativeQueryTests {
+
+    @Autowired
+    EntityManager entityManager;
+
+    private static final long ID_1 = 10001L;
+
+    @Test
+    void nativeQuery_findAllCourses() throws Exception {
+        Query nativeQuery = entityManager.createNativeQuery("select * from courses", Course.class);
+        List resultList = nativeQuery.getResultList();
+        assertTrue(resultList.size() > 0);
+    }
+
+    @Test
+    void nativeQuery_findById_positionalParameter() throws Exception {
+        Query nativeQuery = entityManager.createNativeQuery("select * from courses where ID = ?", Course.class);
+        nativeQuery.setParameter(1, ID_1);
+        List resultList = nativeQuery.getResultList();
+        assertEquals(1, resultList.size());
+    }
+
+    @Test
+    void nativeQuery_findById_namedParameter() throws Exception {
+        Query nativeQuery = entityManager.createNativeQuery("select * from courses where ID = :id", Course.class);
+        nativeQuery.setParameter("id", ID_1);
+        List resultList = nativeQuery.getResultList();
+        assertEquals(1, resultList.size());
+    }
+
+    @Test
+    @Transactional
+    @DirtiesContext
+    void nativeQuery_updateManyRowsAtOnce() throws Exception {
+        Query nativeQuery = entityManager.createNativeQuery("update courses set last_updated_date = LOCALTIMESTAMP()", Course.class);
+        int rowsUpdated = nativeQuery.executeUpdate();
+        assertTrue(rowsUpdated > 0);
+    }
+
+}
